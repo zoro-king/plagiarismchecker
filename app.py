f9296c49681a3a8a2d681352caafd5113c7d7c09
vetrivel <vetrivel200016@gmail.com>
2022-11-19 18:11:43 +0530

    form

--- app.py
+++ app.py
@@ -31,14 +31,14 @@ def load_lottie(url):
     if r.status_code != 200:
         return None
     return r.json()
-"""
+
 # Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-"""
-#local_css("style\style.css")
+
+local_css("style/style.css")
 
 lottie_coding=load_lottie("https://assets1.lottiefiles.com/packages/lf20_axrujg5c.json")
 with st.container():
@@ -54,7 +54,7 @@ with st.container():
     with right_column:
           st_lottie(lottie_coding,height=300,key="coding")
 
-"""
+
 # ---- CONTACT ----
 with st.container():
     st.write("---")
@@ -63,17 +63,18 @@ with st.container():
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-  #  <form action="https://formsubmit.co/svetri2611@gmail.com" method="POST">
-  #      <input type="hidden" name="_captcha" value="false">
-   #     <input type="text" name="name" placeholder="Your name" required>
-    #    <input type="email" name="email" placeholder="Your email" required>
-     #   <textarea name="message" placeholder="Your message here" required></textarea>
-      #  <button type="submit">Send</button>
-   # "</form>
-"""
+    <form action="https://formsubmit.co/svetri2611@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+
     left_column, right_column = st.columns(2)
     with left_column:
         st.markdown(contact_form, unsafe_allow_html=True)
     with right_column:
         st.empty()
-        """
+        
